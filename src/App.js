b91b5b9f6206b60d98e8e46c48f36514f42ad3d4
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2020-05-24 04:33:29 -0400

    refactored app

--- src/App.js
+++ src/App.js
@@ -2,86 +2,19 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Firebase from './firebase'
 import 'firebase/database';
+import Table from "./components/Table/Table";
+import data from './components/data.json';
 
 function App() {
-  const [info, setInfo] = useState({Course: [], Student: []})
-  const [professor, setProfessor] = useState({Name: "", Courses: []})
-  const db = Firebase.database().ref();
-
-  /*
-    sets state for info (holds everything in db) and current professor logged in
-  */
-  useEffect(() => {
-    const handleData = snap => {
-      if (snap.val()) {
-        let sList = [];
-        let cList = [];
-        for (const student in snap.val()["Student"]){
-          sList.push(snap.val()["Student"][student]);
-        }
-        for (const course in snap.val()["Course"]){
-          cList.push(snap.val()["Course"][course]);
-        }
-        setInfo({Course: cList, Student: sList})
-        setProfessor({Name: "Chris Riesbeck", Courses: [cList[0]]})
-      }
-    }
-    db.on('value', handleData, error => alert(error));
-    return () => { db.off('value', handleData); };
-  }, [db]);
-
-  /*
-    gets temperature (trust me, this is the best way to do it rn b/c it gets the most recent temp)
-    in the future, we can just look for temperatures who's datetime matches today's date instead of the most recent date
-  */
-  const getTemp = s => { 
-    for (const student in info.Student){
-      if (s === info.Student[student]["Name"]){
-          let entries = Object.keys(info.Student[student]["record"])
-          if (entries.length !== 0){
-          let max = 0;
-          let dT = "";
-          for (const entry in entries){
-            if (Date.parse(entries[entry]) > max){
-              max = Date.parse(entries[entry]);
-              dT = entries[entry]
-            }
-          }
-          
-          return [info.Student[student]["record"][dT], dT];
-        }
-        else{
-          return (0, "");
-        }
-      }
-    }
-  }
+  const info = {Course: data["Course"], Students: data["Student"]};
+  const professor = {Name: "Chris Riesbeck", Courses: ["Agile Methodology"]};
   
   return (
     <div className="App">
       <h1>Health Passport</h1>
       <header className="App-header">
         <h1>{professor.Name}</h1>
-          {professor.Courses.map((c) => 
-          <div id="table-container">
-            <h2><u>{c["Name"]}</u></h2>
-            <table className="course-table">
-              <thead>
-                <th>Student Name</th>
-                <th>Latest Temperature</th>
-                <th>Date of Temperature</th>
-              </thead> 
-              <tbody>
-                {professor.Courses.map((c) => c["Roster"].map((s) => 
-                  <tr>
-                    <td>{s}</td>
-                    <td>{getTemp(s)[0]["temp"]}</td>
-                    <td>{getTemp(s)[1]}</td> 
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>)}   
+          <Table course={info.Course["EECS394"]} students={info.Students}/>
       </header>
     </div>
   );
---
+++ src/components/Table/Table.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Tooltip } from "@material-ui/core";
+
+const Table = ({course, students}) => {
+
+    const [heatlhyTemp, setHealthy] = useState(96);
+    const getTemp = s => { 
+
+        let keys = Object.keys(students)
+
+        for (var i = 0; i < keys.length; i++){
+            if ( s === students[keys[i]]["netid"]) {
+                let entries = Object.keys(students[keys[i]]["record"])
+                if (entries.length !== 0){
+                    let max = 0;
+                    let dT = "";
+                    for (const entry in entries){
+                        if (Date.parse(entries[entry]) > max){
+                            max = Date.parse(entries[entry]);
+                            dT = entries[entry]
+                        }
+                    }
+                    return [students[keys[i]]["record"][dT], dT];
+                }
+                else {
+                    return ("-", "-");
+                }
+            }
+        }
+
+        
+        return ("-", "-")
+      }
+
+    return(
+        <div id="table-container">
+            <button onClick={() => heatlhyTemp === 96 ? setHealthy(90) : setHealthy(96)}>
+                Change Healthy Temp from {heatlhyTemp}
+            </button>            
+            <caption>{course["Name"]}</caption>
+            <table className="course-table table-bordered">
+            <thead>
+                <th scope="col">Student Name</th>
+                <th scope="col">Latest Temperature</th>
+                <th scope="col">Date of Temperature</th>
+            </thead> 
+            <tbody>
+                    {course["Roster"].map((s) => 
+                        Number(getTemp(s)[0]) > heatlhyTemp ?
+                            <tr>
+                                <td class="unhealthy">{s}</td>
+                                <Tooltip title="temperature not within healthy range" aria-label="temperature not within healthy range"><td>{getTemp(s)[0]}</td></Tooltip>
+                                <td>{getTemp(s)[1]}</td> 
+                            </tr>
+                        
+                        :
+                            <tr>
+                                <td class="healthy">{s}</td>
+                                <Tooltip title="temperature within healthy range" aria-label="temperature within healthy range"><td>{getTemp(s)[0]}</td></Tooltip>
+                                <td>{getTemp(s)[1]}</td> 
+                            </tr>
+                    )}
+            </tbody>
+            </table>    
+        </div>
+    );
+    
+}
+
+export default Table;
